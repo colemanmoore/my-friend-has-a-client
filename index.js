469fd93f97e8c6349b187a22e095c4a72333d516
Coleman Moore <cmoore.jennikayne@gmail.com>
2021-03-17 13:46:27 -0700

    client updates

--- index.js
+++ index.js
@@ -1,17 +1,70 @@
-var socketServerUrl = 'https://myfriendhasaprinter.herokuapp.com';
-var PRINTER_NAME = 'Nick_Canon';
+const socketClient = require('socket.io-client')
+const { exec } = require('child_process')
 
-var socket = require('socket.io-client')(socketServerUrl);
+require('dotenv/config')
+const socketServerUrl = process.env.SOCKET_SERVER_URL
+const NO_DEFAULT_MSG = 'Please set a system default printer using `sudo lpoptions -d [CUPS printer identifier]`'
 
-socket.on('connect', function() {
-    console.log('MYFRIENDHASAPRINTER Server Connected.');
-});
+let printerName = process.env.PRINTER_NAME, socket
 
-socket.on('disconnect', function() {
-    console.log('MYFRIENDHASAPRINTER Server Disconnected. Might be restarting.');
-});
+(async () => {
 
-socket.on('request-print-job', function(data) {
-    console.log('Print job requested', data);
-    exec('lp -d ' + PRINTER_NAME + ' ' + FILE_PATH);
-});
+    try {
+        printerName = await getDefaultSystemPrinter()
+    } catch (err) {
+        console.log('Error getting default system printer', err)
+        return
+    }
+
+    try {
+        socket = socketClient(socketServerUrl)
+    } catch (err) {
+        console.log('Error getting socket', err)
+        return
+    }
+
+    if (printerName && socket) {
+        setupListeners()
+    }
+
+})()
+
+async function getDefaultSystemPrinter() {
+    return new Promise((resolve, reject) => {
+
+        exec('lpstat -s', (err, stdout, stderr) => {
+            const firstLine = stdout.slice('\n')[0]
+            if (!firstLine || firstLine.indexOf('system default destination: ') < 0) {
+                reject(NO_DEFAULT_MSG)
+            }
+
+            resolve(firstLine.split('system default destination: ')[1])
+        })
+    })
+}
+
+function setupListeners() {
+
+    socket.on('connect', () => {
+        console.log(`MYFRIENDHASAPRINTER Server Connected on ${socket.url} : printer = ${printerName}`)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('MYFRIENDHASAPRINTER Server Disconnected. Might be restarting.')
+    })
+
+    socket.on('my-friend-send-file', () => {
+        console.log('MYFRIENDHASAPRINTER File in transit')
+    })
+
+    socket.on('my-friend-request-print', function(data) {
+        console.log('MYFRIENDHASAPRINTER Print job requested', data)
+        if (data.filepath) {
+            try {
+                exec(`lp -d ${printerName} ${data.filepath}`)
+            } catch (err) {
+                console.log('Error executing print command', err)
+            }
+        }
+    })
+}
