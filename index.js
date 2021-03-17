682ef305c668bbed07a8ba6c312e838bba434552
Coleman Moore <cmoore.jennikayne@gmail.com>
2021-03-17 15:19:00 -0700

    socket listening right

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let printerName, socket
 
     if (printerName && socket) {
         setupListeners()
-        console.log(`Listening for print requests on ${socket.io.uri} ...`)
+        console.log(`Listening on ${socket.io.uri} ...`)
     } else {
         console.log(`Failed to listen for print requests: Printer=${printerName}, socket=${socket.io.uri?'defined':'undefined'}`)
     }
@@ -55,28 +55,27 @@ async function getDefaultSystemPrinter() {
 }
 
 function setupListeners() {
-    console.log(socket.on)
 
     socket.on('connect', () => {
-        console.log('hellooooo')
-        console.log(`MYFRIENDHASAPRINTER Server Connected on ${socket.io.uri} : printer = ${printerName}`)
+        console.log('MY FRIEND HAS A PRINTER')
+        console.log(`~~~ Server Connected on ${socket.io.uri} : printer = ${printerName}`)
     })
 
     socket.on('disconnect', () => {
-        console.log('MYFRIENDHASAPRINTER Server Disconnected. Might be restarting.')
+        console.log('~~~ Server Disconnected. Might be restarting.')
     })
 
     socket.on('my-friend-send-file', () => {
-        console.log('MYFRIENDHASAPRINTER File in transit')
+        console.log('~~~ File in transit')
     })
 
     socket.on('my-friend-request-print', function(data) {
-        console.log('MYFRIENDHASAPRINTER Print job requested', data)
+        console.log('~~~ Print job requested', data)
         if (data.filepath) {
             try {
                 exec(`lp -d ${printerName} ${data.filepath}`)
             } catch (err) {
-                console.log('Error executing print command', err)
+                console.log('* * Error executing print command', err)
             }
         }
     })
